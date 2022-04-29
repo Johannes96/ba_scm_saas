0b09fd10a9b7d693c250b6b40ea5cb913fff590f
Bernadett Farkas <bernadett.farkas@dracoon.com>
2022-04-29 14:13:49 +0200

    change

--- modules/yearly_analytics.R
+++ modules/yearly_analytics.R
@@ -1,5 +1,5 @@
 
-descriptive_analytics_UI <- function(id) {
+yearly_analytics_UI <- function(id) {
   ns <- NS(id)
   
   tagList(
@@ -18,19 +18,19 @@ descriptive_analytics_UI <- function(id) {
                                        multiple=TRUE,
                                        selected=character(0)
                         ))),
-        fluidRow(column(5,
-                        # Select which Sales Channel and type
-                        selectInput(inputId = ns("SalesChannelFinder"),
-                                    label = "Select Sales Channel",
-                                    choices=NULL, 
-                                    multiple=TRUE)
-        ),
-        column(5, ofset = 3,
-               selectInput(inputId = ns("SalesTypFinder"),
-                           label = "Select Saley-typ",
-                           choices=NULL, 
-                           multiple=TRUE)
-        )),
+        # fluidRow(column(5,
+        #                 # Select which Sales Channel and type
+        #                 selectInput(inputId = ns("SalesChannelFinder"),
+        #                             label = "Select Sales Channel",
+        #                             choices=NULL, 
+        #                             multiple=TRUE)
+        # ),
+        # column(5, ofset = 3,
+        #        selectInput(inputId = ns("SalesTypFinder"),
+        #                    label = "Select Saley-typ",
+        #                    choices=NULL, 
+        #                    multiple=TRUE)
+        # )),
         
         # Select which Region(s) to plot
         checkboxGroupInput(inputId = "ProductTypeFinder",
@@ -52,26 +52,26 @@ descriptive_analytics_UI <- function(id) {
   
 }
 
-descriptive_analytics <- function(input, output, session) {
-  
-  
-  selected <- reactiveValues(SalesChannel = NULL,
-                             SalesTyp = NULL)
-  
-  filter <- reactiveValues(SalesChannel = unique(saas_data$SalesChannel),
-                           SalesTyp = unique(saas_data$SalesTyp))
-  
-  
-  observeEvent(eventExpr = input$SalesChannelFinder, ignoreNULL = FALSE, ignoreInit = TRUE, {
-    selected$SalesChannel <- input$SalesChannelFinder
-    filter$SalesChannel <- if(is.null(selected$SalesChannel)) unique(saas_data$SalesChannel) else selected$Kart
-  })
-  
-  observeEvent(eventExpr = input$SalesTypFinder, ignoreNULL = FALSE, ignoreInit = TRUE, {
-    selected$Kgrp <- input$SalesTypFinder
-    filter$SalesTyp <- if(is.null(selected$SalesTyp)) unique(saas_data$SalesTyp) else selected$SalesTyp
-  })
-
-# Input Boxes
-updateSelectizeInput(session, "SalesChannelFinder", label="Select Sales Channel", choices = unique(saas_data$SalesChannel), server=TRUE)
-updateSelectizeInput(session, "SalesTypFinder", label="Select Saley-typ", choices = unique(saas_data$SalesTyp), server=TRUE)
+# descriptive_analytics <- function(input, output, session) {
+#   
+#   
+#   selected <- reactiveValues(SalesChannel = NULL,
+#                              SalesTyp = NULL)
+#   
+#   filter <- reactiveValues(SalesChannel = unique(saas_data$SalesChannel),
+#                            SalesTyp = unique(saas_data$SalesTyp))
+#   
+#   
+#   observeEvent(eventExpr = input$SalesChannelFinder, ignoreNULL = FALSE, ignoreInit = TRUE, {
+#     selected$SalesChannel <- input$SalesChannelFinder
+#     filter$SalesChannel <- if(is.null(selected$SalesChannel)) unique(saas_data$SalesChannel) else selected$Kart
+#   })
+#   
+#   observeEvent(eventExpr = input$SalesTypFinder, ignoreNULL = FALSE, ignoreInit = TRUE, {
+#     selected$Kgrp <- input$SalesTypFinder
+#     filter$SalesTyp <- if(is.null(selected$SalesTyp)) unique(saas_data$SalesTyp) else selected$SalesTyp
+#   })
+# 
+# # Input Boxes
+# updateSelectizeInput(session, "SalesChannelFinder", label="Select Sales Channel", choices = unique(saas_data$SalesChannel), server=TRUE)
+# updateSelectizeInput(session, "SalesTypFinder", label="Select Saley-typ", choices = unique(saas_data$SalesTyp), server=TRUE)
