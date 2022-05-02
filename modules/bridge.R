4d9139e04ff3dbd72ac9393b47d838a2701e7ec8
Bernadett Farkas <bernadett.farkas@dracoon.com>
2022-05-02 11:36:57 +0200

    small change

--- modules/bridge.R
+++ modules/bridge.R
@@ -19,10 +19,10 @@ bridge <- pivot_wider(saas_data_temp, names_from = Period, values_from = TotalAR
 bridge[is.na(bridge)] <- 0.00
 TotalARR <- format(round(saas_data_temp$TotalARR, 2), nsmall = 2)
 
-bridge$"Dif_2020-09" <- format(round((bridge$"2020-10-01" - bridge$"2020-09-01"), 2), nsmall = 2)
+bridge$"Dif_2020-10" <- format(round((bridge$"2020-10-01" - bridge$"2020-09-01"), 2), nsmall = 2)
 
 #Create categories
-bridge$"Change_2020-09" <- ifelse((bridge$"2020-09-01" > bridge$"2020-10-01"& bridge$"2020-10-01" != 0), "Contraction",
+bridge$"Change_2020-10" <- ifelse((bridge$"2020-09-01" > bridge$"2020-10-01"& bridge$"2020-10-01" != 0), "Contraction",
                                   ifelse((bridge$"2020-09-01" < bridge$"2020-10-01"& bridge$"2020-09-01" != 0), "Expansion",
                                          ifelse((bridge$"2020-09-01" ==  0 & bridge$"Dif_2020-09" != 0 & bridge$"2020-10-01" != 0), "New",
                                                 ifelse((bridge$"2020-10-01" ==  0 & bridge$"Dif_2020-09" != 0 & bridge$"2020-09-01" != 0), "Churn", "No change"))))
