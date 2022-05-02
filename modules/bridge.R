57ed93ed95f6978cbc6ae882c213dbaedf15f391
Bernadett Farkas <bernadett.farkas@dracoon.com>
2022-05-02 11:33:57 +0200

    bridge categories

--- modules/bridge.R
+++ modules/bridge.R
@@ -12,30 +12,18 @@ saas_data_temp <- saas_data_temp %>%
                   ungroup()
 
 
-# Prepara Bridge ----------------------------------------------------------
-
-TotalARR <- format(round(saas_data_temp$TotalARR, 2), nsmall = 2)
+# Prepare Bridge ----------------------------------------------------------
 
 bridge <- pivot_wider(saas_data_temp, names_from = Period, values_from = TotalARR) 
 
-bridge[is.na(bridge)] <- 0
-
+bridge[is.na(bridge)] <- 0.00
+TotalARR <- format(round(saas_data_temp$TotalARR, 2), nsmall = 2)
 
 bridge$"Dif_2020-09" <- format(round((bridge$"2020-10-01" - bridge$"2020-09-01"), 2), nsmall = 2)
 
-#not correct
-bridge$"Change_2020-09" <- ifelse(bridge$"2020-09-01" > bridge$"2020-10-01", "Contraction",
-                                  ifelse(bridge$"2020-09-01" < bridge$"2020-10-01", "Expansion",
-                                         ifelse((bridge$"2020-09-01" ==  0 & bridge$"Dif_2020-09" != 0), "New",
-                                                ifelse((bridge$"2020-10-01" ==  0 & bridge$"Dif_2020-09" != 0), "Churn", "No change"))))
+#Create categories
+bridge$"Change_2020-09" <- ifelse((bridge$"2020-09-01" > bridge$"2020-10-01"& bridge$"2020-10-01" != 0), "Contraction",
+                                  ifelse((bridge$"2020-09-01" < bridge$"2020-10-01"& bridge$"2020-09-01" != 0), "Expansion",
+                                         ifelse((bridge$"2020-09-01" ==  0 & bridge$"Dif_2020-09" != 0 & bridge$"2020-10-01" != 0), "New",
+                                                ifelse((bridge$"2020-10-01" ==  0 & bridge$"Dif_2020-09" != 0 & bridge$"2020-09-01" != 0), "Churn", "No change"))))
 
-#not correct
-bridge$"Change_2020-09" <- ifelse(bridge$"2020-09-01" > bridge$"2020-10-01", "Contraction",
-                                  ifelse(bridge$"2020-09-01" < bridge$"2020-10-01", "Expansion",
-                                         ifelse(bridge$"2020-09-01" ==  -1*(bridge$"Dif_2020-09"), "Churn",
-                                                ifelse(bridge$"2020-10-01" ==  bridge$"Dif_2020-09", "New", "No change" ))))
-
-
-
-# bridge$"Abs_2020-09" <- bridge$"Dif_2020-09" %>% 
-#                         mutate_if(is.numeric, funs(. * -1))
