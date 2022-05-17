bc536149892a1e32db88257b300b6a0d8bc1c4b8
Johannes96 <j-sach@t-online.de>
2022-05-17 22:51:14 +0200

    tab number of clusters implemented

--- modules/clustering.R
+++ modules/clustering.R
@@ -29,7 +29,19 @@ clustering_UI <- function(id) {
                      
                     )
                  ),
-               tabPanel("Number of Clusters", verbatimTextOutput("summary")),
+               tabPanel(
+                 "Number of Clusters",
+                 fluidRow(
+                   column(6,
+                          
+                          withSpinner(plotOutput(ns("elbow_method")))
+                   ),
+                   column(6,
+                          withSpinner(plotOutput(ns("silhouette_score")))
+                   )
+                   
+                 )
+                 ),
                tabPanel("Interpretation", tableOutput("table"))
              )
         )
@@ -48,6 +60,7 @@ clustering_server <- function(input, output, session) {
   shinyjs::disable("btn_n_cluster")
   shinyjs::disable("btn_interpret")
   
+  cluster_results <- reactiveValues(df1 = 0, kmeans = 0)
   
   observeEvent(input$btn_debug, {
     browser()
@@ -56,10 +69,9 @@ clustering_server <- function(input, output, session) {
   
   
   observeEvent(input$btn_pca, {
-    cat("btn clicked!\n")
-    # disable button after first click
+
+    # disable button after first click and shot Tab
     shinyjs::disable("btn_pca")
-    
     showTab(session = session, inputId = "tabs", target = "PCA", select = TRUE)
     
     data_clustering.pca <- prcomp(data_clustering[, c(6:10)], scale = TRUE)
@@ -68,10 +80,10 @@ clustering_server <- function(input, output, session) {
     df_pca_temp <- data.frame(pca = c("PC1", "PC2", "PC3", "PC4", "PC5"),
                               cum_variance = pca_cum_variance)
     
-    clustering_results <- clustering_results %>%
+    cluster_results$df1 <- data_clustering %>%
       mutate(PC1 = data_clustering.pca$x[,1],
              PC2 = data_clustering.pca$x[,2])
-      
+
     output$pca_cum_variance <- renderPlot({
       p_cum_var <- ggplot(df_pca_temp, aes(x=pca, y=cum_variance, group=1, label = scales::percent(cum_variance))) +
         geom_line() +
@@ -80,8 +92,8 @@ clustering_server <- function(input, output, session) {
         labs(x="Principal Components",y="Cumulative explained Variance") + 
         ggtitle("Cumulative Explained Variance per Principal Component") +
         theme(aspect.ratio=1/1)
-      
-      shinyjs::enable("btn_n_cluster")
+      print(p_cum_var)
+    
     })
     
     output$pca_biplot <- renderPlot({
@@ -96,7 +108,7 @@ clustering_server <- function(input, output, session) {
     })
     
     #TODO: ggf. Tabelle einfügen
-    
+    shinyjs::enable("btn_n_cluster")
       
   })
   
@@ -104,27 +116,61 @@ clustering_server <- function(input, output, session) {
   
   observeEvent(input$btn_n_cluster, {
     
+    shinyjs::disable("btn_n_cluster")
+    showTab(session = session, inputId = "tabs", target = "Number of Clusters", select = TRUE)
+    
     calc_kmeans <- function(x) {
-      km_temp <- kmeans(clustering_results[,11:12], centers = x, nstart = 10)
+      km_temp <- kmeans(cluster_results$df1[,11:12], centers = x, nstart = 10)
       
-      # append new column to existing data frame
-      #n_cluster <- rep(x, nrow(clustering_results))
-      clustering_results[ , ncol(clustering_results) + 1] <- km_temp$cluster
-      colnames(clustering_results)[ncol(clustering_results)] <- paste0("n_cluster_", x)
-      browser()
-      #clustering_results <- cbind(clustering_results, km_temp$cluster)
+      # append cluster to existing data frame
+      cluster_results$df1[ , ncol(cluster_results$df1) + 1] <- km_temp$cluster
+      colnames(cluster_results$df1)[ncol(cluster_results$df1)] <- paste0("n_cluster_", x)
       return(km_temp)
     }
     
-    kmeans_results <- sapply(1:10, calc_kmeans)
+    cluster_results$kmeans <- sapply(1:10, calc_kmeans)
+    
+    # calculate silhouette score for 1 to 10 clusters
+    calc_ss <- function(x) {
+      #browser()
+      ss <- cluster::silhouette(cluster_results$kmeans[,x]$cluster, dist(cluster_results$df1[,11:12]))
+      return(mean(ss[, 3]))
+    }
+    
+    avg_ss <- sapply(2:10, calc_ss)
+
+    # calculate total within sum of squares for 1 to 10 clusters
+    tot_wss <- sapply(2:10, function(x) {cluster_results$kmeans[,x]$tot.withinss})
+
+    df_plot <- data.frame(n_cluster = 2:10,
+                          avg_ss = avg_ss,
+                          tot_wss = tot_wss)
+    
+    output$elbow_method <- renderPlot({
+      p_tot_wss <- ggplot(df_plot, aes(x=n_cluster, y=tot_wss, group=1)) +
+        geom_line() +
+        geom_point() +
+        labs(x="Number of Clusters", y="Total Within Sum of Squares") + 
+        ggtitle("Elbow Method")
+      print(p_tot_wss)
+      
+    })
+    
+    output$silhouette_score <- renderPlot({
+      p_ss <- ggplot(df_plot, aes(x=n_cluster, y=avg_ss, group=1)) +
+        geom_line() +
+        geom_point() +
+        labs(x="Number of Clusters", y="Silhoette Score") + 
+        ggtitle("Silhouette Score")
+      print(p_ss)
+      
+    })
     
     shinyjs::enable("btn_interpret")
     
   })
   
+  #TODO: implement interpretation (scatterplot, choose number of clusters, cond. boxplots/violinplots)
   
   
 }
-
-# TODO: 
-# Solve bug that tooltip is freezed on button click... add tooltips again?
